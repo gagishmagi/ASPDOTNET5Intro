9b95d7c2c28a4219cf23fe020c50498c4137a13a
Gagi Shmagi <gagi.shmagi@gmail.com>
2022-01-02 14:00:11 +0200

    Initial Commit

---
+++ ASPDOTNET5MVCIntro/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ASPDOTNET5MVCIntro.Models;
+
+namespace ASPDOTNET5MVCIntro.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogDebug("Debug: This is from my logger");
+            _logger.LogInformation("This is from my logger");
+            // ViewData["Title"] = "Home Page 2";
+            ViewBag.Title = "Home Page 3";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
